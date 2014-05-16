c1354a70c928bbe431839f4028532e973bc0e93c
Jeremy Falling <projects@falling.se>
2014-05-16 16:16:40 -0700

    .

--- plugins/whitelist-filter.pm
+++ plugins/whitelist-filter.pm
@@ -8,6 +8,7 @@ sub whitelist.filter {
 	use NetAddr::IP;
 
 
+	#you can do either cdir notation, range (x.x.x.x-x.x.x.x), or single ips
         if ($ip->within(new NetAddr::IP "192.168.15.0/24")) {
 
 		debugOutput("**DEBUG: $ip is in whitelist");
