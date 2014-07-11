20e97c9d20e2bbf583845a8e6de5ec393e74a430
Jeremy Falling <projects@falling.se>
2014-07-10 18:50:59 -0700

    added time status for plugins, starting to migrate debug output/logging

--- autoban.pl
+++ autoban.pl
@@ -31,6 +31,8 @@ use Fcntl qw(LOCK_EX LOCK_NB);
 use File::NFSLock;
 use Cwd 'abs_path';
 use File::Basename;
+use Time::HiRes qw(usleep ualarm gettimeofday tv_interval);
+use Log::Log4perl qw(:easy);
 
 
 #get offical elasticsearch module @ https://metacpan.org/pod/Search::Elasticsearch
@@ -50,18 +52,19 @@ my $configFile = "$autobanPath/autoban.cfg";
 #define program version
 my $autobanVersion = "0.0.1";
 
-my ($help, $man, $foreground, $debug, $verbose, $version, $daemon);
-our $safe;
+my ($help, $man, $foreground, $color, $loglevel, $version, $daemon);
+our $safe; #<-- this should probally not be handled as a global....
 my @plugins;
 
-#TODO: fix whole debug vs verbose thing. 
+#TODO: fix whole debug vs verbose thing and replace with logging system
+my ($verbose, $debug);
 Getopt::Long::Configure('bundling');
 GetOptions
     ('h|help|?' => \$help, 
      'man' => \$man,
+     "C|color" => \$color,
      "d|debug" => \$debug,
-     "D|daemon" => \$daemon,
-     "V|verbose" => \$verbose,     
+     "l|loglevel=s" => \$loglevel,
      'f|foreground' => \$foreground,
      "v|version" => \$version,
      "s|safe" => \$safe) or pod2usage(2);
@@ -74,39 +77,62 @@ if ($version) {
     exit;
 }
 
-# Before we do anything else, try to get an exclusive lock
-my $lock = File::NFSLock->new($0, LOCK_EX|LOCK_NB); 
 
-#unless we are running in the foreground, die if there is another copy running
-unless ($foreground) {
-    die "\nERROR: I am already running and I will not run another demonized copy!\nTo run manually while the daemon is running, give the foreground flag. See help or the man page\n\n" unless $lock;
-}
-
-
-#TEMP, this is to force me to use the foreground flag to work on the daemon mode
-unless ($foreground) {
-  die "Sorry, daemon mode is not yet implimented, use the foreground flag (see --man/-h)\n\n";
-}
+our $autobanConfig = new Config::Simple(filename=>"$configFile"); #<-- this should probally not be handled as a global....
 
 #check if config file exists, and if not exit
 unless (-e $configFile) {
-    print "\nERROR: $configFile was found! Please see the man page!\n";
+    print "\nFATAL: $configFile was found! Please see the man page (--man)!\n";
     exit 1;
 }
 
-#this needs to not be a global....
-our $autobanConfig = new Config::Simple(filename=>"$configFile");
-
-
-print "\n\n";
-print "Starting autoban v.$autobanVersion, please wait...\n\n";
-
-if ($debug){
-    print "\n\n**DEBUG: Debug output enabled\n";
+#set the log level via the flag or fall back to the config
+my $autobanLogLevel;
+if ($loglevel){
+    $autobanLogLevel = $loglevel;
 }
-elsif ($verbose){
-    print "\n\nVerbose output enabled\n";
+else {
+    $autobanLogLevel = $autobanConfig->param('autoban.LogLevel');
 }
+
+# Before we do anything else, try to get an exclusive lock
+my $lock = File::NFSLock->new($0, LOCK_EX|LOCK_NB); 
+
+#set up the logging.
+if ($foreground) {
+
+    #we are running in the foreground then log to stdout and continue running. 
+
+
+    #control color output depending on user request
+    if ($color){
+
+      setupLogging();
+  Log::Log4perl->init(\ <<'EOT');
+log4perl.appender.Screen = \
+	     Log::Log4perl::Appender::ScreenColoredLevels
+EOT
+    }
+    else{
+  Log::Log4perl->init(\ <<'EOT');
+log4perl.appender.Screen = \
+	     Log::Log4perl::Appender::Screen
+EOT
+    }
+
+}
+else {
+    #we are not running in the foreground check if there is another copy of this program running and die if so
+    die "\nFATAL: I am already running and I will not run another demonized copy!\nTo run manually while the daemon is running, give the foreground flag. See help or the man page\n\n" unless $lock;
+
+    #not running another copy, setup logging
+    Log::Log4perl->easy_init( { level   => $autobanLogLevel,
+                                 file    => ">>$autobanConfig->param('autoban.Logfile')"} );
+}
+
+#Any output past this point should be handled by the logging system
+FATAL("Starting autoban v.$autobanVersion, please wait...");
+
 
 
 #check if running as root, if so give warning.
@@ -143,7 +169,7 @@ my $autobanIndexStatus = $es->indices->exists(
     index   => $autobanConfig->param('autoban.esAutobanIndex')
     );
 unless ($autobanIndexStatus) {
-    print "WARNING: autboan's index (", $autobanConfig->param('autoban.esAutobanIndex'), ") was not found. Creating it.\n";
+    print "WARNING: autboan's index (", $autobanConfig->param('autoban.esAutobanIndex'), ") was not found. This is normal if this is the first time running autoban, otherwise something delete it. I am creating the index in elasticsearch now.\n";
     die "ERROR: could not create autoban index..." unless $es->indices->create(index=> $autobanConfig->param('autoban.esAutobanIndex'));
     enhancedOutput("debug","**DEBUG: autoban index created");
 }
@@ -175,14 +201,22 @@ foreach my $runPlugin ($autobanConfig->param('autoban.runPlugins')) {
     #work around strict not allowing string as a subroutine ref
     my $subref = \&$runPlugin;
 
+    #get time just before running module
+    my $modPluginTime = [gettimeofday];
+
     #try to run the function
     &$subref();
+    
+    #get amt of time the plugin took to run
+    my $elapsedPluginTime = tv_interval ($modPluginTime);
+    enhancedOutput("debug","**DEBUG: Plugin $runPlugin took $elapsedPluginTime seconds to run");
 
 
 }
 
 
-#This function will be used to give the user output, if they so desire
+#This function will be used to give the user output, if they so desire.
+#TODO:  This should be replaced by some logging module as I will need to do logging to files
 sub enhancedOutput {
     #we get two inputs, first is the type of message, second is the message
     my $outputType = $_[0];
@@ -198,6 +232,22 @@ sub enhancedOutput {
 }
 
 
+sub setupLogging {
+  Log::Log4perl->init(\ <<'EOT');
+             log4perl.category = DEBUG, Screen
+             log4perl.appender.Screen = \
+	     Log::Log4perl::Appender::ScreenColoredLevels
+             log4perl.appender.Screen.layout = \
+	     Log::Log4perl::Layout::PatternLayout
+             log4perl.appender.Screen.layout.ConversionPattern = \
+	     %d %F{1} %L> %m %n
+EOT
+
+
+}
+
+
+
 __END__
 
 =head1 NAME
@@ -209,19 +259,19 @@ autoban - Realtime attack and abuse defence and intrusion prevention
 autoban [options]
 
      Options:
-       -h,-help         brief help message
-       --man            full documentation
-       -d,--debug       enable debug mode
-       -V,--verbose     enable verbose messages
+       -C,--color       enabled colored text (in foreground mode)
        -D,--daemon      run as a daemon
        -f,--foreground  run in foreground
+       -h,-help         brief help message
+       -l,--loglevel    logging level
+       --man            full documentation
        -s,--safe        safe mode
        -v,--version     display version
 
 =head1 DESCRIPTION
 
 B<autoban> is used to analyze inputs, apply filters and push data to outputs
-
+If using a flag that sets an option that is also set in the config, the flag value will be used. 
 
 =head1 OPTIONS
 
@@ -229,34 +279,35 @@ No options are required
 
 =over 8
 
-=item B<-h, --help>
-Print a brief help message and exits.
-
-=item B<--man>
-Print the manual page.
-
-=item B<-d, --debug> 
-Enable debug mode with a rather high amount of output (debug lines start with **DEBUG). This will supercede the verbose flag
-
-=item B<-V, --verbose> 
-Enable verbose messages
+=item B<-C, --color> 
+Use colored text when running in foreground mode
 
 =item B<-D, --daemon> 
 Run as a daeon
 
 =item B<-f, --foreground>
-Run in foreground. This will enable you to run autoban in the foreground, even if the daemon is running.
+Run in foreground. This will enable you to run autoban in the foreground, even if the daemon is running. This will also cause messages to go to stdout instead of the log file. 
+
+=item B<-h, --help>
+Print a brief help message and exits.
+
+=item B<-l, --loglevel [log level]> 
+Specify logging level (TRACE, DEBUG, INFO, WARN, ERROR, FATAL)
+
+=item B<--man>
+Print the manual page.
 
 =item B<-s,--safe>
-Run in safe mode. This will not preform any bans, but instead display what would have happened. This is useful if you want to run this in read only mode. This will invoke verbose mode (-v)
+Run in safe mode. This will not preform any bans, but instead display what would have happened. This is useful if you want to run this in read only mode. This will invoke info level logging
 
 =item B<-v, --version> 
 Display program version 
+
 
 =back
 
 =head1 CHANGELOG
 
-B<0.1> 12-10-2013 Initial release. All other releases until there is a stable product will be under this version. 
+B<0.1> 12-10-2013 Initial release. All future releases until there is a stable product will be under this version. 
 
 =cut
